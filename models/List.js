c210d12397af306dfbc63a299ced2118ddbe34d1
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-28 21:51:25 +0200

    setup Mongoose schema and model

---
+++ models/List.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const listSchema = new Schema({
+  user: String,
+  createdAt: Date,
+  updatedAt: Date,
+  items: [{
+    title: String,
+    description: String,
+    completed: Boolean,
+  }]
+});
+
+const List = model('List', listSchema);
+export default List;
--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require("express")
 const app = express();
 const PORT = 3000;
 
+const mongoose = require('mongoose');
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
+
+app.use(express.json());
 
 app.get("/", async(req, res) => {
   res.send("Hello from todo-mongo!");
