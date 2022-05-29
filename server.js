7db3b817cc3ca72fc2fa1a05122889912fdac4f9
lspangenberg <laurens.am.spangenberg@gmail.com>
2022-05-29 17:37:22 +0200

    add get endpoints

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 
 const mongoose = require('mongoose');
+const List = require("./models/List");
 mongoose.connect('mongodb+srv://dbAdmin:lb63hneyWNalFZhj@cluster0.bsw1yaz.mongodb.net/?retryWrites=true&w=majority')
 
 app.listen(PORT, () => {
@@ -13,4 +14,24 @@ app.use(express.json());
 
 app.get("/", async(req, res) => {
   res.send("Hello from todo-mongo!");
+})
+
+app.get("/lists", async(req, res) => {
+  try {
+    const getLists = await List.find();
+    res.send(getLists);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
+})
+
+app.get("/lists/:user", async(req, res) => {
+  try {
+    const getList = await List.findOne({user: req.params.user});
+    res.send(getList);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("something went wrong");
+  }
 })
